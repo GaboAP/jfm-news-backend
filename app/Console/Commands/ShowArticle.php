15366980727c0d934633131f2ec341c7a6289158
Gabriel Omar Almendras Pena <gapen@jfm.dk>
2025-08-28 11:50:46 -0400

    First commit, added artisan commands

---
+++ app/Console/Commands/ShowArticle.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ShowArticle extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'article:show';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
