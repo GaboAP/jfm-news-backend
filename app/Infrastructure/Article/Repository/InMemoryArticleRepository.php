8b372baef6684f8e9c9784613502f204605752e4
Gabriel Omar Almendras Pena <gapen@jfm.dk>
2025-09-04 12:58:30 -0400

    Added inmemoryarticle repository

---
+++ app/Infrastructure/Article/Repository/InMemoryArticleRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Infrastructure\Article\Repository;
+
+use App\Domain\Article\Contract\ArticleRepositoryInterface;
+use App\Domain\Article\Article;
+use App\Domain\Shared\ArticleId;
+
+final class InMemoryArticleRepository implements ArticleRepositoryInterface
+{
+    /** @var array<string, Article> */
+    private array $store = [];
+
+    public function save(Article $article): void
+    {
+        $this->store[$article->id()->toString()] = $article;
+    }
+
+    public function get(ArticleId $id): ?Article
+    {
+        return $this->store[$id->toString()] ?? null;
+    }
+}
